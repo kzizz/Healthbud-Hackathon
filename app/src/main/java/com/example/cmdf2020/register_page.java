c865f327c842922c6dd60a508f6aa1467d9baf26
kzizz <krystenz@telus.net>
2020-03-07 22:27:35 -0800

    Added transitions between login and sign up page

--- app/src/main/java/com/example/cmdf2020/register_page.java
+++ app/src/main/java/com/example/cmdf2020/register_page.java
@@ -2,13 +2,25 @@ package com.example.cmdf2020;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class register_page extends AppCompatActivity {
+     TextView btnLogin;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_page);
+        btnLogin = findViewById(R.id.signIn);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(register_page.this, MainActivity.class));
+            }
+        });
     }
 }
