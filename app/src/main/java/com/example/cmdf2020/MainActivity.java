bed5a0f3338a9ca574239059b4007f55ef494adb
Jamalzadeh, Mojan <mojan.jamalzadeh@philips.com>
2020-03-07 21:59:13 -0800

    log in functionality is okay

--- app/src/main/java/com/example/cmdf2020/MainActivity.java
+++ app/src/main/java/com/example/cmdf2020/MainActivity.java
@@ -12,10 +12,15 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 
 public class MainActivity extends AppCompatActivity {
 
