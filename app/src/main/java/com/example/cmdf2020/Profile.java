09e5ad6ac3afae207a5be9ae218676e3d106c7f4
Julie Belleville <35176177+jbellevi@users.noreply.github.com>
2020-03-08 05:08:27 -0700

    added default constructor to profile

--- app/src/main/java/com/example/cmdf2020/Profile.java
+++ app/src/main/java/com/example/cmdf2020/Profile.java
@@ -25,9 +25,72 @@ public class Profile {
                    int age, int fitness_lvl, int workout_freq,
                    boolean is_mom, boolean is_senior, boolean is_student, boolean is_injured) {
         this.db = db; // assume a firestore database object is passed in
+        this.name = name;
+        this.workout_pref = workout_pref;
+        this.city = city;
+        this.age = age;
+        this.fitness_lvl = fitness_lvl;
+        this.workout_freq = workout_freq;
+        this.is_mom = is_mom;
+        this.is_senior = is_senior;
+        this.is_student = is_student;
+        this.is_injured = is_injured;
+    }
+
+    public Profile(FirebaseFirestore db) {
+        this.db = db; // assume a firestore database object is passed in
+        this.name = "name";
+        this.workout_pref = "workout_pref";
+        this.city = "city";
+        this.age = 0;
+        this.fitness_lvl = 0;
+        this.workout_freq = 0;
+        this.is_mom = true;
+        this.is_senior = true;
+        this.is_student = true;
+        this.is_injured = true;
     }
 
     public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+        // TODO: update database
+    }
+
+    public String getWorkout_pref() {
+        return workout_pref;
+    }
+
+    public void setWorkout_pref(String pref) {
+        this.workout_pref = pref;
+        // TODO: update database
+    }
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String name) {
+        this.city = city;
+        // TODO: update database
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+        // TODO: update database
+    }
+    public int getFitness_lvl() {
+        return fitness_lvl;
+    }
+
+    public void setFitness_lvl(int fitness) {
+        this.fitness_lvl = fitness;
+        // TODO: update database
     }
 }
